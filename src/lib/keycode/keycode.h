7785d32018eddb638ec50c950215420a6d8c31d4
ilmars <9987548+darksworm@users.noreply.github.com>
2019-11-10 19:08:02 +0200

    rm keycode submodule

--- src/lib/keycode/keycode.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "depp-keycode/src/keytable.h"
-
-std::string linux_rawname_to_yaml_name(std::string linux_rawname);
-
-std::string linux_keycode_to_yaml_name(unsigned linux_keycode);
